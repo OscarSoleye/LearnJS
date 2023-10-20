d1c490b844c60945b7efa6ea918332a29775ab41
Oscar Soleye <clouddavinci@gmail.com>
2023-10-20 19:23:42 +0100

    First Commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10)
+
+console.log("Wale");
+console.log(23)
+
+let firstName = "Wale"
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+//Variable name conventions
+let wale_soleye = "WS"
+let $function = 27 //Only non letters allowed to begin variable names are # and $
+
+let person = 'wale'
+let PI = 3.1415
+
+console.log(wale_soleye)*/
+
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof javascriptisfun)
+console.log()
